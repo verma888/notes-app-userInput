ef536c529e77ffee45811f726b99600eeb412c85
Gourav Verma <51471939+verma888@users.noreply.github.com>
2019-10-07 15:20:27 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,47 @@
+var yargs = require('yargs');
+var fs = require('fs');
+var notesU = require('./notesuti');
+yargs.command({
+    command: "add",
+    describe: "adding a note",
+    builder: {
+        title:{
+            describe: "Title of Note",
+            demandOption: true,
+            type: 'string'
+        },
+        body:{
+            describe: 'Body of Note',
+            demandOption: true,
+            type: 'string'
+        }
+
+    },
+    handler: function(argv){
+        notesU.addNote(argv.title, argv.body);
+    }
+})
+yargs.command({
+    command: "remove",
+    describe: "removing a note",
+    builder: {
+        title: {
+            describe: "title of note",
+            demandOption : true,
+            type : 'string'
+        }
+
+    },
+    handler:  function(argv){
+        notesU.removeNote(argv.title);
+    }
+})
+
+yargs.command({
+    command: "list",
+    description: "list the notes",
+    handler: function(){
+        console.log("this gonna list the notes");
+    }
+})
+yargs.parse();
---
+++ notesuti.js
@@ -0,0 +1,47 @@
+var fs = require('fs');
+
+const getNotes = function(){
+    return "Your notes are....";
+}
+const addNote = function(title, body){
+    const notes = loadNotes();
+    const duplicateNotes = notes.filter(function(note){
+        return note.title === title;
+    })
+    if(duplicateNotes.length==0){
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes);
+    console.log("New note added!");
+    }
+    else {
+        console.log("Title already taken!")
+    }
+}
+
+const saveNotes = function(notes){
+    const DataJson = JSON.stringify(notes);
+    fs.writeFileSync("n-1.json", DataJson);
+}
+
+const loadNotes = function(){
+    try {
+            const dataBuffer = fs.readFileSync('n-1.json');
+            const JsonData = dataBuffer.toString();
+            return JSON.parse(JsonData);
+        } catch (e){
+            return []
+        }
+}
+
+const removeNote = function(title){
+    console.log("Note with title " + title +" has been removed");
+}
+
+module.exports= {
+    getNotes : getNotes,
+    addNote : addNote,
+    removeNote : removeNote
+}
